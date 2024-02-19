9269a6a2974bd3e502c54b04970c07de419896cd
Kazuya Yamamoto <konkoro0503@gmail.com>
2024-02-19 23:24:46 +0900

    Feature/yolobot (#1)
    * Add yolo bot script
    
    * Update gpt publisher
    
    * Fix gpy bot
    
    * Fix error
    
    * Add vision bot
    
    * Fix error
    
    * Update library ver
    
    * Fix gpt yolo
    
    * Fix yolo bot
    
    * Update README
    
    ---------
    
    Co-authored-by: Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# akari_yolo_gpt_bot
+# akari_chatgpt_vision_bot
 
-AKARIの音声対話botにYOLOの三次元物体認識の結果を追加で渡すアプリ
+AKARIの音声対話botに画像認識機能を追加するアプリ
 
 ## submoduleの更新
 `git submodule update --init --recursive`  
@@ -10,4 +10,119 @@ AKARIの音声対話botにYOLOの三次元物体認識の結果を追加で渡�
 `source venv/bin/activate`  
 `pip install -r requirements.txt`  
 
-## 使い方
+## セットアップ方法
+[akari_chatgpt_botのREADME](https://github.com/AkariGroup/akari_chatgpt_bot/blob/main/README.md)のセットアップ手順に沿って実行する。
+
+## 音声対話bot(GPT-4V版)
+
+### 概要
+
+![GPT-4V版図解](jpg/gpt-4v.jpg "GPT-4V版図解")
+
+[akari_chatgpt_bot](https://github.com/AkariGroup/akari_chatgpt_bot)の遅延なし音声対話botに、GPT-4Vによる画像認識機能を追加したものです。  
+
+### 全体図
+
+![構成図](jpg/system_gpt4v.jpg "構成図")
+
+### 起動方法
+
+1. [akari_chatgpt_botのREADME](https://github.com/AkariGroup/akari_chatgpt_bot/blob/main/README.md)内 **遅延なし音声対話botの実行** の起動方法1.~3.を実行する。  
+
+2. gpt_vision_publisherを起動する。(ChatGPTへ画像と文章のリクエストを送信し、受信結果をvoicevox_serverへ渡す。)  
+   `python3 gpt_vision_publisher.py`  
+
+
+   引数は下記が使用可能  
+   - `--ip`: gpt_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--port`: gpt_serverのポート。デフォルトは"10001"
+
+3. speech_publisher.pyを起動する。(Google音声認識の結果をgpt_publisherへ渡す。)  
+   **--no_motionオプションをつけること(つけないと音声認識中にうなずきが再生されてしまい、画像が正しく取得できません。)**
+   `python3 speech_publisher.py --no_motion`  
+
+
+   引数は下記が使用可能  
+   - `--robot_ip`: akari_motion_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--robot_port`: akari_motion_serverのポート。デフォルトは"50055"
+   - `--gpt_ip`: gpt_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--gpt_port`: gpt_serverのポート。デフォルトは"10001"
+   - `--voicevox_ip`: voicevox_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--voicevox_port`: voicevox_serverのポート。デフォルトは"10002"
+   - `-t`,`--timeout`: マイク入力がこの時間しきい値以下になったら音声入力を打ち切る。デフォルトは0.5[s]。短いと応答が早くなるが不安定になりやすい。  
+   - `-p`,`--power_threshold`: マイク入力の音量しきい値。デフォルトは0で、0の場合アプリ起動時に周辺環境の音量を取得し、そこから音量しきい値を自動決定する。  
+   - `--no_motion`: このオプションをつけた場合、音声入力中のうなずき動作を無効化する。  
+
+
+4. カメラの画像表示をするウィンドウが起動したら、`speech_publisher.py`のターミナルでEnterキーを押し、マイクに話しかけるとカメラ画像の表示されているウィンドウに基づいた返答が返ってくる。  
+
+### スクリプトで一括起動する方法
+
+1. [akari_chatgpt_botのREADME](https://github.com/AkariGroup/akari_chatgpt_bot/blob/main/README.md)内 **VOICEVOXをOSS版で使いたい場合** の手順を元に、別PCでVoicevoxを起動しておく。  
+
+2. スクリプトを実行する。  
+
+   `cd script`  
+   `./vision_chatbot.sh {1.でVoicevoxを起動したPCのIPアドレス} {akari_motion_serverのパス}`  
+
+   akari_motion_serverのパスを入力しなければ、akari_motion_serverは起動せず、モーションの再生は行われません(AKARI以外でも使えます)。  
+
+3. カメラの画像表示をするウィンドウが起動したら、`speech_publisher.py`のターミナルでEnterキーを押し、マイクに話しかけるとカメラ画像の表示されているウィンドウに基づいた返答が返ってくる。  
+
+## 音声対話bot(YOLO版)
+
+### 概要
+
+[akari_chatgpt_bot](https://github.com/AkariGroup/akari_chatgpt_bot)の遅延なし音声対話botに、YOLOの3次元位置物体認識の認識結果を組み合わせてChatGPTに送信する機能を追加したものです。  
+
+### 全体図
+
+![構成図](jpg/system_yolo.jpg "構成図")
+
+### 起動方法
+
+1. [akari_chatgpt_botのREADME](https://github.com/AkariGroup/akari_chatgpt_bot/blob/main/README.md)内 **遅延なし音声対話botの実行** の起動方法1.~3.を実行する。  
+
+2. gpt_yolo_publisherを起動する。(ChatGPTへ画像と文章のリクエストを送信し、受信結果をvoicevox_serverへ渡す。)  
+   `python3 gpt_yolo_publisher.py`  
+
+
+   引数は下記が使用可能  
+   - `-m`, `--model`: オリジナルのYOLO認識モデル(.blob)を用いる場合にパスを指定。引数を指定しない場合、YOLO v4のCOCOデータセット学習モデルを用いる。  
+   - `-c`, `--config`: オリジナルのYOLO認識ラベル(.json)を用いる場合にパスを指定。引数を指定しない場合、YOLO v4のCOCOデータセット学習ラベルを用いる。  
+   - `-f`, `--fps`: カメラ画像の取得PFS。デフォルトは8。OAK-Dの性質上、推論の処理速度を上回る入力を与えるとアプリが異常終了しやすくなるため注意。  
+   - `--ip`: gpt_serverのIPアドレス。デフォルトは"127.0.0.1"  
+   - `--port`: gpt_serverのポート。デフォルトは"10001"  
+
+3. speech_publisher.pyを起動する。(Google音声認識の結果をgpt_publisherへ渡す。)  
+   **--no_motionオプションをつけること(つけないと音声認識中にうなずきが再生されてしまい、画像が正しく取得できません。)**
+   `python3 speech_publisher.py --no_motion`  
+
+
+   引数は下記が使用可能  
+   - `--robot_ip`: akari_motion_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--robot_port`: akari_motion_serverのポート。デフォルトは"50055"
+   - `--gpt_ip`: gpt_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--gpt_port`: gpt_serverのポート。デフォルトは"10001"
+   - `--voicevox_ip`: voicevox_serverのIPアドレス。デフォルトは"127.0.0.1"
+   - `--voicevox_port`: voicevox_serverのポート。デフォルトは"10002"
+   - `-t`,`--timeout`: マイク入力がこの時間しきい値以下になったら音声入力を打ち切る。デフォルトは0.5[s]。短いと応答が早くなるが不安定になりやすい。  
+   - `-p`,`--power_threshold`: マイク入力の音量しきい値。デフォルトは0で、0の場合アプリ起動時に周辺環境の音量を取得し、そこから音量しきい値を自動決定する。  
+   - `--no_motion`: このオプションをつけた場合、音声入力中のうなずき動作を無効化する。  
+
+
+4. カメラの画像表示をするウィンドウが起動したら、`speech_publisher.py`のターミナルでEnterキーを押し、マイクに話しかけるとカメラ画像のウィンドウに表示されている物体認識結果に基づいた返答が返ってくる。  
+
+### スクリプトで一括起動する方法
+
+1. [akari_chatgpt_botのREADME](https://github.com/AkariGroup/akari_chatgpt_bot/blob/main/README.md)内 **VOICEVOXをOSS版で使いたい場合** の手順を元に、別PCでVoicevoxを起動しておく。  
+
+2. スクリプトを実行する。  
+
+   `cd script`  
+   `./yolo_chatbot.sh {1.でVoicevoxを起動したPCのIPアドレス} {akari_motion_serverのパス}`  
+
+   akari_motion_serverのパスを入力しなければ、akari_motion_serverは起動せず、モーションの再生は行われません(AKARI以外でも使えます)。  
+
+3. カメラの画像表示をするウィンドウが起動したら、`speech_publisher.py`のターミナルでEnterキーを押し、マイクに話しかけるとカメラ画像のウィンドウに表示されている物体認識結果に基づいた返答が返ってくる。  
+
---
+++ gpt_yolo_publisher.py
@@ -0,0 +1,183 @@
+import argparse
+
+import os
+import sys
+import openai
+import grpc
+import threading
+from concurrent import futures
+from typing import Any
+from akari_chatgpt_bot.lib.chat import create_message
+from akari_chatgpt_bot.lib.chat_akari_grpc import ChatStreamAkariGrpc
+from akari_chatgpt_bot.lib.conf import OPENAI_APIKEY
+import copy
+import cv2
+from lib.akari_yolo_lib.oakd_tracking_yolo import OakdTrackingYolo
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/grpc"))
+import gpt_server_pb2
+import gpt_server_pb2_grpc
+import voicevox_server_pb2
+import voicevox_server_pb2_grpc
+
+# OAK-D LITEの視野角
+fov = 56.7
+
+
+class YoloTracking(object):
+    def __init__(
+        self,
+        config_path: str,
+        model_path: str,
+        fps: int,
+        fov: float,
+    ) -> None:
+        self.oakd_tracking_yolo = OakdTrackingYolo(
+            config_path=config_path, model_path=model_path, fps=fps, fov=fov
+        )
+        self.tracklets = []
+        self.labels = self.oakd_tracking_yolo.get_labels()
+
+    def set_tracklet(self, tracklets: Any) -> None:
+        self.tracklets = tracklets
+
+    def get_result_text(self) -> str:
+        text = " 認識結果 {\n"
+        if self.tracklets is not None:
+            for tracklet in self.tracklets:
+                if tracklet.status.name != "NEW" and tracklet.status.name != "TRACKED":
+                    continue
+                text += f"種類: {self.labels[tracklet.label]},"
+                text += "あなたから見た位置:"
+                if tracklet.spatialCoordinates.x >= 0:
+                    text += "右"
+                else:
+                    text += "左"
+                text += "{:.2f}メートル".format(abs(tracklet.spatialCoordinates.x) / 1000)
+                if tracklet.spatialCoordinates.y >= 0:
+                    text += "上"
+                else:
+                    text += "下"
+                text += "{:.2f}メートル".format(abs(tracklet.spatialCoordinates.y) / 1000)
+                text += "近さ {:.2f}メートル".format(
+                    abs(tracklet.spatialCoordinates.z) / 1000
+                )
+                text += "\n"
+        text += "}"
+        return text
+
+
+class GptServer(gpt_server_pb2_grpc.GptServerServiceServicer):
+    """
+    chatGPTにtextを送信し、返答をvoicevox_serverに送るgprcサーバ
+    """
+
+    def __init__(self, yolo_tracking: YoloTracking):
+        content = "チャットボットとしてロールプレイします。あかりという名前のカメラロボットとして振る舞ってください。物体の認識結果は、カメラロボットであるあなたから見た距離です。質問の内容によっては回答に使ってください。"
+        self.messages = [create_message(content, role="system")]
+        voicevox_channel = grpc.insecure_channel("localhost:10002")
+        self.stub = voicevox_server_pb2_grpc.VoicevoxServerServiceStub(voicevox_channel)
+        self.chat_stream_akari_grpc = ChatStreamAkariGrpc()
+        self.yolo_tracking = yolo_tracking
+
+    def SetGpt(
+        self, request: gpt_server_pb2.SetGptRequest(), context: grpc.ServicerContext
+    ) -> gpt_server_pb2.SetGptReply:
+        response = ""
+        if len(request.text) < 2:
+            return gpt_server_pb2.SetGptReply(success=True)
+        print(f"Receive: {request.text}")
+        if request.is_finish:
+            request.text += self.yolo_tracking.get_result_text()
+            content = f"{request.text}。回答は一文で短くまとめて答えてください。"
+        else:
+            content = f"「{request.text}」という文に対して、以下の「」内からどれか一つを選択して、それだけ回答してください。\n「えーと。」「はい。」「うーん。」「いいえ。」「はい、そうですね。」「そうですね…。」「いいえ、違います。」「こんにちは。」「ありがとうございます。」「なるほど。」「まあ。」"
+        tmp_messages = copy.deepcopy(self.messages)
+        tmp_messages.append(create_message(content))
+        if request.is_finish:
+            for sentence in self.chat_stream_akari_grpc.chat(tmp_messages, model="gpt-4"):
+                print(f"Send voicevox: {sentence}")
+                self.stub.SetVoicevox(
+                    voicevox_server_pb2.SetVoicevoxRequest(text=sentence)
+                )
+                self.messages.append(create_message(response, role="assistant"))
+                response += sentence
+        else:
+            for sentence in self.chat_stream_akari_grpc.chat_and_motion(tmp_messages):
+                print(f"Send voicevox: {sentence}")
+                self.stub.SetVoicevox(
+                    voicevox_server_pb2.SetVoicevoxRequest(text=sentence)
+                )
+                response += sentence
+        return gpt_server_pb2.SetGptReply(success=True)
+
+    def SendMotion(
+        self, request: gpt_server_pb2.SendMotionRequest(), context: grpc.ServicerContext
+    ) -> gpt_server_pb2.SendMotionReply:
+        success = self.chat_stream_akari_grpc.send_motion()
+        return gpt_server_pb2.SendMotionReply(success=success)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="Provide model name or model path for inference",
+        default="yolov4_tiny_coco_416x416",
+        type=str,
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Provide config path for inference",
+        default="json/yolov4-tiny.json",
+        type=str,
+    )
+    parser.add_argument(
+        "-f",
+        "--fps",
+        help="Camera frame fps. This should be smaller than nn inference fps",
+        default=8,
+        type=int,
+    )
+    parser.add_argument(
+        "--ip", help="Gpt server ip address", default="127.0.0.1", type=str
+    )
+    parser.add_argument(
+        "--port", help="Gpt server port number", default="10001", type=str
+    )
+    args = parser.parse_args()
+    yolo_tracking = YoloTracking(
+        config_path=args.config,
+        model_path=args.model,
+        fps=args.fps,
+        fov=fov,
+    )
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    gpt_server_pb2_grpc.add_GptServerServiceServicer_to_server(
+        GptServer(yolo_tracking), server
+    )
+    server.add_insecure_port(args.ip + ":" + args.port)
+    server.start()
+    print(f"gpt_publisher start. port: {args.port}")
+    while True:
+        frame = None
+        detections = []
+        try:
+            frame, detections, tracklets = yolo_tracking.oakd_tracking_yolo.get_frame()
+        except BaseException:
+            pass
+        if tracklets is not None:
+            yolo_tracking.set_tracklet(tracklets)
+        if frame is not None:
+            yolo_tracking.oakd_tracking_yolo.display_frame(
+                "nn", frame, yolo_tracking.tracklets
+            )
+        if cv2.waitKey(1) == ord("q"):
+            end = True
+            break
+
+
+if __name__ == "__main__":
+    main()
