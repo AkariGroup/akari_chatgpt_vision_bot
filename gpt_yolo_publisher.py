07b4ab7de95bf7252c43205dc75107b4648cd11a
Kazuya Yamamoto <konkoro0503@gmail.com>
2024-03-10 23:50:02 +0900

    Feature/claude (#2)
    * Add claude
    
    * Fix model
    
    ---------
    
    Co-authored-by: Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>

--- gpt_yolo_publisher.py
+++ gpt_yolo_publisher.py
@@ -1,17 +1,15 @@
 import argparse
-
+import copy
 import os
 import sys
-import openai
-import grpc
 import threading
 from concurrent import futures
 from typing import Any
-from akari_chatgpt_bot.lib.chat import create_message
-from akari_chatgpt_bot.lib.chat_akari_grpc import ChatStreamAkariGrpc
-from akari_chatgpt_bot.lib.conf import OPENAI_APIKEY
-import copy
+
 import cv2
+import grpc
+import openai
+from akari_chatgpt_bot.lib.chat_akari_grpc import ChatStreamAkariGrpc
 from lib.akari_yolo_lib.oakd_tracking_yolo import OakdTrackingYolo
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "lib/grpc"))
@@ -73,12 +71,14 @@ class GptServer(gpt_server_pb2_grpc.GptServerServiceServicer):
     """
 
     def __init__(self, yolo_tracking: YoloTracking):
-        content = "チャットボットとしてロールプレイします。あかりという名前のカメラロボットとして振る舞ってください。物体の認識結果は、カメラロボットであるあなたから見た距離です。質問の内容によっては回答に使ってください。"
-        self.messages = [create_message(content, role="system")]
         voicevox_channel = grpc.insecure_channel("localhost:10002")
         self.stub = voicevox_server_pb2_grpc.VoicevoxServerServiceStub(voicevox_channel)
         self.chat_stream_akari_grpc = ChatStreamAkariGrpc()
         self.yolo_tracking = yolo_tracking
+        content = "チャットボットとしてロールプレイします。あかりという名前のカメラロボットとして振る舞ってください。物体の認識結果は、カメラロボットであるあなたから見た距離です。質問の内容によっては回答に使ってください。"
+        self.messages = [
+            self.chat_stream_akari_grpc.create_message(content, role="system")
+        ]
 
     def SetGpt(
         self, request: gpt_server_pb2.SetGptRequest(), context: grpc.ServicerContext
@@ -91,17 +91,22 @@ class GptServer(gpt_server_pb2_grpc.GptServerServiceServicer):
             request.text += self.yolo_tracking.get_result_text()
             content = f"{request.text}。回答は一文で短くまとめて答えてください。"
         else:
-            content = f"「{request.text}」という文に対して、以下の「」内からどれか一つを選択して、それだけ回答してください。\n「えーと。」「はい。」「うーん。」「いいえ。」「はい、そうですね。」「そうですね…。」「いいえ、違います。」「こんにちは。」「ありがとうございます。」「なるほど。」「まあ。」"
+            content = f"{request.text}。"
         tmp_messages = copy.deepcopy(self.messages)
-        tmp_messages.append(create_message(content))
+        tmp_messages.append(self.chat_stream_akari_grpc.create_message(content))
         if request.is_finish:
-            for sentence in self.chat_stream_akari_grpc.chat(tmp_messages, model="gpt-4"):
+            self.messages = copy.deepcopy(tmp_messages)
+            for sentence in self.chat_stream_akari_grpc.chat(
+                tmp_messages, model="gpt-4"
+            ):
                 print(f"Send voicevox: {sentence}")
                 self.stub.SetVoicevox(
                     voicevox_server_pb2.SetVoicevoxRequest(text=sentence)
                 )
-                self.messages.append(create_message(response, role="assistant"))
                 response += sentence
+            self.messages.append(
+                self.chat_stream_akari_grpc.create_message(response, role="assistant")
+            )
         else:
             for sentence in self.chat_stream_akari_grpc.chat_and_motion(tmp_messages):
                 print(f"Send voicevox: {sentence}")
@@ -114,7 +119,7 @@ class GptServer(gpt_server_pb2_grpc.GptServerServiceServicer):
     def SendMotion(
         self, request: gpt_server_pb2.SendMotionRequest(), context: grpc.ServicerContext
     ) -> gpt_server_pb2.SendMotionReply:
-        success = self.chat_stream_akari_grpc.send_motion()
+        success = self.chat_stream_akari_grpc.send_reserved_motion()
         return gpt_server_pb2.SendMotionReply(success=success)
 
 
